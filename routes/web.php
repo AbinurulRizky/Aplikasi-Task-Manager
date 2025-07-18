54ea7e471be6542bc46b31a5c9f6645c35cc8c9c
Abinurul Rizky <abinurulrizky@gmail.com>
2025-07-18 09:56:23 +0700

    Tampilan untuk login dan regiter form.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('loginform.login');
+});
+
+Route::get('/login', function() {
+    return view('loginform.login');
+});
+
+route::get('/register', function () {
+    return view('loginform.register');
 });
